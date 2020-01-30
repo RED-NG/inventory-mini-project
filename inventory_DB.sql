7f8c15c5cf11104807679475272bc869e1520084
Philip Ng <phil__n@hotmail.com>
2020-01-29 21:16:59 -0500

    stuff

--- inventory_DB.sql
+++ inventory_DB.sql
@@ -10,3 +10,12 @@ USE inventory_DB;
      supplier  VARCHAR(50) NOT NULL,
      PRIMARY KEY(id)
  )
+
+ INSERT INTO products (name, quantity, price, supplier)
+VALUES ("Celery", 100, 3.00, "Mister Produce");
+
+ INSERT INTO products (name, quantity, price, supplier)
+VALUES ("Broccoli", 50, 5.00, "Mister Produce");
+
+ INSERT INTO products (name, quantity, price, supplier)
+VALUES ("Carrot", 71, 1.48, "Mister Produce");
