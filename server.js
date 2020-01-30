1dfbf46c4f964d39fde56a2ba57b3c0511ed78db
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-01-29 20:38:24 -0500

    set up api

--- server.js
+++ server.js
@@ -1,49 +1,110 @@
+var exphbs = require("express-handlebars");
 var express = require("express");
-
-var PORT = process.env.PORT || 8080;
+var mysql = require("mysql");
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body as JSON
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "codingbootcamp1",
+  password: "root",
   database: "inventory_DB"
 });
 
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM inventory;", function(err, data) {
-    if (err) {
-      throw err;
-    }
-
-    res.render("index", { products: data });
-  });
-});
-
-// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
+});
+
+// Serve index.handlebars to the root route.
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM products;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { products: data });
+  });
+});
+
+// Show the user the individual quote and the form to update the quote.
+app.get("/:id", function(req, res) {
+  connection.query("SELECT * FROM products where id = ?", [req.params.id], function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    console.log(data);
+    res.render("single-product", data[0]);
+  });
+});
+
+app.post("/api/addproduct", function(req, res) {
+  connection.query("INSERT INTO products (name, quantity, price, supplier) VALUES (?, ?, ?, ?)", [req.body.name, req.body.quantity, req.body.price, req.body.supplier], function(
+    err,
+    result
+  ) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+app.delete("/api/delproduct/:id", function(req, res) {
+  connection.query("DELETE FROM products WHERE id = ?", [req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+// Update a quote by an id and then redirect to the root route.
+app.put("/api/products/:id", function(req, res) {
+  connection.query(
+    "UPDATE products SET name = ?, quantity= ?, price = ?, supplier = ? WHERE id = ?",
+    [req.body.name, req.body.quantity, req.body.price, req.body.supplier, req.params.id],
+    function(err, result) {
+      if (err) {
+        // If an error occurred, send a generic server failure
+        return res.status(500).end();
+      }
+      else if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
 });
 
 // Start our server so that it can begin listening to client requests.
