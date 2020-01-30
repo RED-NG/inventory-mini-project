5620d08e759195f4f235134ca2664c0448802051
Neal Bhatt <nealyb@gmail.com>
2020-01-29 21:19:59 -0500

    server details

--- connection.js
+++ connection.js
@@ -7,8 +7,8 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password:
-      database:
+      password: 'Fucktouch12',
+      database: 'inventory_DB'
     });
   };
 
